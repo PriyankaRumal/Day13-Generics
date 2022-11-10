a1827368b1c859c0a1ca65794d12cf715ae28211
PriyankaRumal <priyankarumal182000@gmail.com>
2022-11-10 12:05:42 -0800

    solution added

---
+++ Generic/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Generic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("maximum no is");
+            int output = MaximumIntNumber.MaximumIntNum(11, 25, 33);
+            Console.WriteLine(output);
+        }
+    }
+}
